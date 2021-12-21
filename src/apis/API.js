23c84f63edeff43b6c5eee6828d4b6a1d8d78f15
mritscsi <95491737+mritscsi@users.noreply.github.com>
2021-12-21 14:08:10 +0530

    BUG FIX

--- src/apis/API.js
+++ src/apis/API.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default axios.create({
-	baseURL: 'https://csimrits.tech/api/v1/',
+  baseURL: "http://173.230.139.42:8000/api/v1/",
 });
